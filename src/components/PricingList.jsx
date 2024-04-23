0633369c5658872f40bd80cd5f6359740ed7cad9
Yash Bishnoi <87704585+Yash111416@users.noreply.github.com>
2024-04-23 13:26:53 +0530

    components and constats added

---
+++ src/components/PricingList.jsx
@@ -0,0 +1,43 @@
+import { pricing } from "../constants";
+
+import {check, rupee} from "../assets"
+import Button from "./Button";
+
+
+const PricingList = () => {
+  return (
+    <div className="flex gap-[1rem] max-lg:flex-wrap">
+        {pricing.map((items)=> (
+           <div key={items.id} className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border
+             border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2
+             [&>h4]:even:text-color-1 [&>h4]:last:text-color-3  ">
+            <h4 className="h4 mb-4 ">{items.title}</h4>
+            <p className="body-2 min-h-[4rem] mb-3 text-n-1/50 ">{items.description}</p>
+            <div className="flex items-center h-[5.5rem] mb-6">
+                {items.price && (
+                   <>
+                   <div className="h3">₹</div>
+                    <div className="text-[5.5rem] leading-none font-bold">{items.price}</div>
+                    </>
+                )}
+            </div>
+            <Button className="w-full mb-6" href={items.price ? '/pricing': 'mailto:yashbishnoi95@gmail.com'}
+                white={!!items.price}>
+                {items.price ? 'Get Started': 'Contact Us'}
+            </Button>
+            <ul>
+                {items.features.map((feature, index) =>  (
+                   <li className="flex items-start py-5 border-t border-n-6 " key={index}>
+                    <img src={check} width={24} height={24} alt="Check" />
+                    <p className="body-2 ml-4">{feature}</p>
+                   </li> 
+                ))}
+            </ul>
+           </div> 
+        ))}
+
+    </div>
+  )
+}
+
+export default PricingList;
